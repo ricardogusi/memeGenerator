f67444c52628c87582741d42647857535ecea2e2
ricardogusi <ricardogusi@hotmail.com>
2020-07-16 12:40:58 -0300

    Updates

---
+++ precache-manifest.252dfa4aa9bc290b95f9bef8a709157e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7aa7f856763ef60900aa3930f5d33648",
+    "url": "/memeGenerator/index.html"
+  },
+  {
+    "revision": "4bdb5fedcea437f0ccdf",
+    "url": "/memeGenerator/static/css/main.f54ce5ff.chunk.css"
+  },
+  {
+    "revision": "7e85a168b4fdb72fff3f",
+    "url": "/memeGenerator/static/js/2.40a6b46a.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/memeGenerator/static/js/2.40a6b46a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4bdb5fedcea437f0ccdf",
+    "url": "/memeGenerator/static/js/main.0ae9bfac.chunk.js"
+  },
+  {
+    "revision": "1615fa5acab025faed75",
+    "url": "/memeGenerator/static/js/runtime-main.77f762b5.js"
+  },
+  {
+    "revision": "34849ca749c6e12d74b790be95d138e5",
+    "url": "/memeGenerator/static/media/bg.34849ca7.jpg"
+  }
+]);
